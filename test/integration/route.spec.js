3781993a541c4e36f687bd14af39675fe5e3b038
designeng <denissavenok@gmail.com>
2015-12-09 21:10:34 +0300

    problems with .post testing

--- test/integration/route.spec.js
+++ test/integration/route.spec.js
@@ -14,7 +14,19 @@ describe('integration', function () {
         apitest.get('/api/test').expect(200, done);
     });
 
-    it('model.json path exists', function (done) {
+    it('model.json path exists and should accept get request', function (done) {
         apitest.get('/model.json?paths=[["names","length"]]&method=get').expect(200, done);
+    });
+
+    // TODO
+    xit('model.json should accept post request', function (done) {
+        apitest.post('/model.json')
+            // .send({ method    : 'call' })
+            // .send({ callPath  : ["names", "add"]})
+            // .send({ arguments : ["1234567"] })
+            .field( 'method', 'call' )
+            // .field( callPath  : ["names", "add"] )
+            // .field( arguments : ["1234567"] )
+            .expect(200, done);
     });
 });
