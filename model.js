ffd1de0936a9e63cd055d22199ae41d585209a96
designeng <denissavenok@gmail.com>
2015-12-09 20:37:53 +0300

    around aspect added to model 'get' method

--- model.js
+++ model.js
@@ -1,7 +1,17 @@
-var Falcor = require('falcor'),
-    FalcorDataSource = require('falcor-http-datasource'),
-    model = new Falcor.Model({
-        source: new FalcorDataSource('/model.json')
-    });
+var Falcor = require('falcor');
+var FalcorDataSource = require('falcor-http-datasource');
+var meld = require('meld');
+
+var model = new Falcor.Model({
+    source: new FalcorDataSource('/model.json')
+});
+
+meld.around(model, "call", function (joinpoint) {
+    console.log("around:", joinpoint);
+
+    var callPath    = joinpoint.args[0];
+    var args        = joinpoint.args[1];
+    return joinpoint.proceed(callPath, args);
+});
 
 module.exports = model
